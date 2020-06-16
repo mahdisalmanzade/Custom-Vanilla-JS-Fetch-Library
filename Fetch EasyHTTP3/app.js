09b31e760039f53b6af4c239d3593061647b6422
MahdiSalmanizadegan <55497711+mahdisalmanzade@users.noreply.github.com>
2020-06-16 11:36:59 +0430

    Add files via upload

---
+++ Fetch EasyHTTP3/app.js
@@ -0,0 +1,31 @@
+ // Instantiating easyHTTP Class
+// const http = new EasyHTTP;
+
+// http.get('https://jsonplaceholder.typicode.com/users')
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
+
+// // const data = {
+// //   name: 'John Doe',
+// //   username: 'JohnDoe',
+// //   email: 'jdoe@gmail.com'
+// // }
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// const data = {
+//   name: 'John Doe',
+//   username: 'JohnDoe',
+//   email: 'jdoe@gmail.com'
+// }
+
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// Delete User
+// http.delete('user.json')
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
---
+++ Fetch EasyHTTP3/easyhttp3.js
@@ -0,0 +1,65 @@
+/**
+ *  EasyHTTP Library
+ *  Library for Making HTTP Request
+ * @version 3.0.0
+ * @author Mahdi SLM
+ * @license MIT
+ * 
+ * 
+ */
+class EasyHTTP {
+  // Make an HTTP GET Request
+  async get(url) {
+    
+    const response = await fetch(url);
+    //  Only proceed if promise resolved
+     const resData = await response.json();
+     return resData;
+      
+    }
+  
+
+  // Make And HTTP POST Request
+  async post(url, data) {
+   
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+      const resData = await response.json();
+      return resData;
+    };
+  
+
+  // Make an HTTP PUT Request
+  async put(url, data) {
+    
+      const response = await fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+      const resData = await response.json();
+      return resData;
+    };
+  
+  // Make an HTTP DELETE Request
+  async delete(url) {
+  
+     const response = await fetch(url, {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json'
+        }
+      });
+
+      const resData = await response.json();
+      return resData;
+    };
+  
+}
